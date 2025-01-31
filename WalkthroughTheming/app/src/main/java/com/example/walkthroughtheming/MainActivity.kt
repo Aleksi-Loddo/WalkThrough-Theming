067ce5fe4e3954f92694847b4fcfb55d1e83da8e
aleksi loddo <143383551+Aleksi-Loddo@users.noreply.github.com>
2025-01-31 15:28:21 +0200

    theming
    walkthrough  theming exercise

---
+++ WalkthroughTheming/app/src/main/java/com/example/walkthroughtheming/MainActivity.kt
@@ -0,0 +1,71 @@
+package com.example.walkthroughtheming
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.walkthroughtheming.ui.theme.WalkthroughThemingTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            WalkthroughThemingTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    MyApp(
+
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun MyApp( modifier: Modifier = Modifier) {
+    val appModifier = Modifier.fillMaxWidth().padding(horizontal = 8.dp)
+    Column (
+        modifier = modifier,
+        verticalArrangement = Arrangement.spacedBy(16.dp)
+    ) {
+        Text(
+            text = "My Title",
+            modifier = appModifier,
+            style = MaterialTheme.typography.titleLarge
+        )
+        OutlinedTextField(
+            value = "",
+            onValueChange = {/*Todo*/ },
+            modifier = appModifier
+        )
+        Button(onClick = { /*TODO*/ },
+            modifier = appModifier) {
+            Text(text = "Submit")
+
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    WalkthroughThemingTheme {
+        MyApp()
+    }
+}
